4d91314afffe48878f996b96ced356f517ccd8b4
venson <venson.hunt@gmail.com>
2025-03-07 15:09:53 +0800

    finish

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(count: u16) -> u16 {
+    if count > 40 {
+        count * 1
+    } else {
+        count * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
